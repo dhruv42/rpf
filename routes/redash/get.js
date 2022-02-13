06b67bb3e5f7e28170674b607b1c655c1e5fe943
Rahul <rahul@setu.co>
2022-02-13 18:52:13 +0530

    filter redash data on biller

--- routes/redash/get.js
+++ routes/redash/get.js
@@ -4,23 +4,30 @@ const { messages, statusCode } = require("../../constants/constants.json");
 const { responseHandler } = require("../../helpers/requestResponse");
 
 const fetchData = async (req, res) => {
-    try {
-        const options = {
-          uri: `${config.REDASH_URL}/api/queries/271/results`,
-          method: "GET",
-          json: true,
-          headers: {
-            Authorization: config.REDASH_API_KEY,
-          },
-        };
-        const response = await rp(options);
-        res.status(statusCode.OK).json(responseHandler(true, statusCode.OK, messages.SUCCESS, response));
-    } catch (error) {
-        console.log(error);
-        res.status(statusCode.BAD_REQUEST).json(responseHandler(false, statusCode.BAD_REQUEST, messages.BAD_REQUEST, error));
+  const biller = req.params['biller'];
+  try {
+    const options = {
+      //  uri: `${config.REDASH_URL}/api/queries/271/results`,
+      uri: `${config.REDASH_URL}/api/queries/714/results`,
+      method: "GET",
+      json: true,
+      headers: {
+        Authorization: config.REDASH_API_KEY,
+      },
+    };
+    const response = await rp(options);
+    const dataRows = response.query_result.data.rows;
+    const responseRows = []
+    for (const row of dataRows) {
+      if (row.biller.toUpperCase().includes(biller.toUpperCase()))
+        responseRows.push(row)
     }
+    res.status(statusCode.OK).json(responseHandler(true, statusCode.OK, messages.SUCCESS, responseRows));
+  } catch (error) {
+    console.log(error);
+    res.status(statusCode.BAD_REQUEST).json(responseHandler(false, statusCode.BAD_REQUEST, messages.BAD_REQUEST, error));
+  }
 };
-
 module.exports = {
   fetchData,
 };
